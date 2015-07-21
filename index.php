09d8fed70be0f60e8060045c62fff1b850cb7928
Dong Dung <tiendungdong@gmail.com>
2015-07-21 12:04:01 +0000

    test c9.io

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
  * Date: 21/07/2015
  * Time: 18:36
  */
-echo "<p>me</p>";
+echo "<p>HI me</p>";
 $arr = array(1,2,3,4);
 var_dump($arr);
--- test.php
+++ test.php
@@ -5,4 +5,10 @@
  * Date: 21/07/2015
  * Time: 18:56
  */
-echo "this is test file. Hello";
+echo "this is test file. Hello <br />";
+$arr = array("a", "b", "c");
+$arr2 = array("e","f");
+
+$result = array_merge($arr, $arr2);
+
+var_dump($result);
