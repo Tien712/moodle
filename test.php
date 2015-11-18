8c5320a85a50a8de4e9d596cdfced302035ca1eb
rinodung <tiendungdong@gmail.com>
2015-11-18 19:18:19 +0700

    reset function 2

--- test.php
+++ test.php
@@ -5,7 +5,11 @@
  * Date: 21/07/2015
  * Time: 18:56
  */
+<<<<<<< HEAD
 echo "This is commit need to revert. Hello <br />";
+=======
+echo "this is test file. Hello <br />";
+>>>>>>> parent of 5f93630... revert function
 $arr = array("a", "b", "c");
 $arr2 = array("e","f");
 
