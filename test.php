4c8bac55b836b5ac20af94251f3751aa27fd606b
rinodung <tiendungdong@gmail.com>
2015-11-18 19:19:20 +0700

    uipdate reset function

--- test.php
+++ test.php
@@ -6,10 +6,14 @@
  * Time: 18:56
  */
 <<<<<<< HEAD
+<<<<<<< HEAD
 echo "This is commit need to revert. Hello <br />";
 =======
 echo "this is test file. Hello <br />";
 >>>>>>> parent of 5f93630... revert function
+=======
+echo "this is test file. Hello <br />";
+>>>>>>> parent of 8becd07... reset function
 $arr = array("a", "b", "c");
 $arr2 = array("e","f");
 
